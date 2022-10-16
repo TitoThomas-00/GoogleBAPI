e900cd8e1593db536313621bd88f4bce64cd96c3
titothomas-00 <thomastito88@gmail.com>
2022-10-16 12:17:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/thirdtime/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.thirdtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResults;
+    private RequestQueue mQueue;
+    EditText inputText; //Accepts ISBN 10 and ISBN 13 (SHOULD ONLY ACCEPT #### , RESTRICT ALPHABET)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResults = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+        inputText = (EditText) findViewById(R.id.textfield_1);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+
+    }
+
+    private void jsonParse() {
+
+
+        //String url = "https://booksrun.com/api/price/sell/"+inputText.getText()+"?key=t2xlhotmhy246sby0zvu"; //Book Runs API
+        String url = "https://www.googleapis.com/books/v1/volumes?q=" + inputText.getText(); //Google Books API
+
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray itemsArray = response.getJSONArray("items");
+                            //for (int i = 0; i < itemsArray.length(); i++) {
+                                JSONObject itemsObj = itemsArray.getJSONObject(0); //To get first index
+                                JSONObject volumeObj = itemsObj.getJSONObject("volumeInfo");
+                                String title = volumeObj.optString("title");
+                                JSONArray authorsArray = volumeObj.getJSONArray("authors");
+                                String description = volumeObj.optString("description");
+                                int pageCount = volumeObj.optInt("pageCount");
+                                ArrayList<String> authorsArrayList = new ArrayList<>();
+                                if (authorsArray.length() != 0) {
+                                    for (int j = 0; j < authorsArray.length(); j++) {
+                                        authorsArrayList.add(authorsArray.optString(0));
+                                    }
+
+                                /* //FOR BOOKRUNS API
+                               JSONObject jsonObject = response.getJSONObject("result");
+                                //for(int i = 0;i<jsonObject.length();i++) {
+                                String status = jsonObject.getString("status");
+                                //String message = jsonObject.getString("message");
+                                JSONObject text = jsonObject.getJSONObject("text"); //Prices
+                                mTextViewResults.append(status+" , "+ text);
+
+                                 */ //FOR BOOKRUNS API
+                                    mTextViewResults.append("\n\n\n"+title + ", " + authorsArrayList + "\n\n"+"pages:"+pageCount+",\n"+description);
+
+
+                                }
+
+
+                        }catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+
+
+        });
+
+        mQueue.add(request);
+    }
+}
